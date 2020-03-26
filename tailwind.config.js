d3d4cea1b295372222469a9b83ac2cc3f50e6fb3
hdriqi <ra@evius.id>
2020-03-26 19:14:08 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  corePlugins: {
+    fontFamily: false
+  },
+  theme: {
+    extend: {
+      colors: {
+        'black-1': '#1B1B1B',
+        'black-2': '#3F3F3F',
+        'black-3': '#616161',
+        'black-4': '#8F8F8F',
+        'black-5': '#BFBFBF',
+        'black-6': '#DFDFDF'
+      },
+      padding: {
+        'half': '50%'
+      }
+    }
+  }
+}
