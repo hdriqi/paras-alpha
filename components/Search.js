dbfd658067ed351cb76ded42934514ca64082a3b
hdriqi <ra@evius.id>
2020-05-12 11:47:31 +0700

    add fuzzy search

--- components/Search.js
+++ components/Search.js
@@ -1,11 +1,9 @@
-import { useState, useEffect, useImperativeHandle } from "react"
+import { useState } from "react"
 import { withRedux } from "../lib/redux"
-import { useSelector, useDispatch, batch } from "react-redux"
 import axios from "axios"
-import { useRouter } from "next/router"
-import { addData } from "../actions/me"
 import Push from "./Push"
 import Pop from "./Pop"
+import Fuse from 'fuse.js'
 
 const Search = () => {
   const [userList, setUserList] = useState([])
@@ -15,8 +13,15 @@ const Search = () => {
 
   const _getUsers = async (query) => {
     if(query.length > 0) {
-      const response = await axios.get(`http://localhost:3004/users?username_like=${query}`)
-      setUserList(response.data)
+      const userList = await axios.get(`http://localhost:3004/users?username_like=${query}`)
+      const mementoList = await axios.get(`http://localhost:3004/blocks?name_like=${query}`)
+      const combinedList = userList.data.concat(mementoList.data)
+      const fuse = new Fuse(combinedList, {
+        includeScore: true,
+        keys: ['username', 'name']
+      })
+      const result = fuse.search(query).map(res => res.item)
+      setUserList(result)
     }
   }
 
@@ -29,8 +34,8 @@ const Search = () => {
   }
 
   return (
-    <div className="fixed bg-white-1 inset-0 z-30">
-      <div className="pb-16">
+    <div className="bg-white-1 min-h-screen">
+      <div className="pb-12">
         <div className="fixed bg-white shadow-subtle top-0 left-0 right-0 h-12 px-4">
           <div className="relative w-full h-full flex items-center">
             <div>
@@ -48,25 +53,47 @@ const Search = () => {
       </div>
       <div>
         {
-          userList && userList.map(user => {
+          userList.map(data => {
             return (
-              <div key={user.id} className="flex items-center bg-white shadow-subtle mt-4 p-4 overflow-hidden">
-                <div>
-                  <div className="w-10 h-10 rounded-full overflow-hidden">
-                    <img style={{
-                      boxShadow: `0 0 4px 0px rgba(0, 0, 0, 0.75) inset`
-                    }} className="object-cover w-full h-full" src={user.avatarUrl} />
-                  </div>
-                </div>
-                <div className="px-4 w-auto">
-                  <Push href="/[username]" as={ `/${user.username}` } props={{
-                    username: user.username,
-                    user: user
-                  }}>
-                    <p className="font-semibold text-black-1 truncate whitespace-no-wrap min-w-0">{ user.username }</p>
-                  </Push>
-                  <p className="text-black-3 text-sm truncate whitespace-no-wrap min-w-0">{ user.bio }</p>
-                </div>
+              <div key={data.id}>
+                {
+                  data.username ? (
+                    <Push href="/[username]" as={ `/${data.username}` } props={{
+                      username: data.username,
+                      user: data
+                    }}>
+                      <div className="flex items-center bg-white shadow-subtle mt-4 p-4 overflow-hidden">
+                        <div>
+                          <div className="w-10 h-10 rounded-full overflow-hidden">
+                            <img style={{
+                              boxShadow: `0 0 4px 0px rgba(0, 0, 0, 0.75) inset`
+                            }} className="object-cover w-full h-full" src={data.avatarUrl} />
+                          </div>
+                        </div>
+                        <div className="px-4 w-auto">
+                          <p className="font-semibold text-black-1 truncate whitespace-no-wrap min-w-0">{ data.username }</p>
+                          <p className="text-black-3 text-sm truncate whitespace-no-wrap min-w-0">{ data.bio }</p>
+                        </div>
+                      </div>
+                    </Push>
+                  ) : (
+                    <Push href="/m/[id]" as={ `/m/${data.id}` } props={{
+                      id: data.id
+                    }}>
+                      <div className="flex items-center bg-white shadow-subtle mt-4 p-4 overflow-hidden">
+                        <div>
+                          <div className='flex items-center w-8 h-8 rounded-full overflow-hidden bg-black-1'>
+                            <div className='w-4 h-4 m-auto bg-white'></div>
+                          </div>
+                        </div>
+                        <div className='px-4 w-auto'>
+                          <p className='font-semibold text-black-1 truncate whitespace-no-wrap min-w-0'>{ data.name }</p>
+                          <p className='text-black-3 text-sm truncate whitespace-no-wrap min-w-0'>{ data.desc }</p>
+                        </div>
+                      </div>
+                    </Push>
+                  )
+                }
               </div>
             )
           })
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import Search from '../components/Search'
-import NavMobile from '../components/NavMobile'
 
 const SearchScreen = () => {
   return (
-    <div>
-      <Search />
-      <div className="fixed bottom-0 right-0 left-0 z-20">
-        <NavMobile />
-      </div>
-    </div>
+    <Search />
   )
 }
 
