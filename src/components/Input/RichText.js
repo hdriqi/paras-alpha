41311bcd31782d9ae2a74d8fca8bcf0a4ee582b6
hdriqi <ra@evius.id>
2020-06-04 17:55:37 +0700

    create RichText component

---
+++ src/components/Input/RichText.js
@@ -0,0 +1,105 @@
+import { MentionsInput, Mention } from 'react-mentions'
+import near from '../../lib/near'
+import Image from 'components/Image'
+
+const textareaStyle = {
+  control: {
+    fontSize: `16px`,
+    fontWeight: `500`,
+    color: '#616161'
+  },
+  highlighter: {
+    letterSpacing: `-.01rem`
+  },
+  input: {
+    margin: 0,
+    padding: `0 .5rem`,
+    color: `white`,
+    height: `100%`
+  },
+  suggestions: {
+    marginTop: `1rem`,
+    maxHeight: `8rem`,
+    overflowY: 'auto',
+    width: `100vw`,
+    maxWidth: `100%`,
+    boxShadow: `0px 0px 4px rgba(0, 0, 0, 0.15)`,
+    item: {
+      backgroundColor: `#121212`,
+      '&focused': {
+        backgroundColor: '#333333',
+      },
+    },
+  },
+}
+
+const RenderUser = (entry) => {
+  return (
+    <div className="flex items-center justify-between p-2">
+      <div className="w-8/12 flex items-center overflow-hidden">
+        <div>
+          <div className="w-8 h-8 rounded-full overflow-hidden">
+            <Image style={{
+              boxShadow: `0 0 4px 0px rgba(0, 0, 0, 0.75) inset`
+            }} className="object-cover w-full h-full" data={entry.imgAvatar} />
+          </div>
+        </div>
+        <div className="px-4 w-auto">
+          <p className="text-sm font-semibold text-white truncate whitespace-no-wrap min-w-0">{entry.username}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const RichText = ({ text, setText, inputRef, autoFocus }) => {
+  const _getUsers = async (query, callback) => {
+    if (!query) return
+    const q = [`username_like:=${query}`]
+    const userList = await near.contract.getUserList({
+      query: q,
+      opts: {
+        _embed: true,
+        _sort: 'createdAt',
+        _order: 'desc',
+        _limit: 10
+      }
+    })
+    const list = userList.map(user => ({
+      display: `@${user.username}`,
+      id: user.id,
+      imgAvatar: user.imgAvatar,
+      username: user.username
+    }))
+    callback(list)
+  }
+
+  const _onChange = (e) => {
+    setText(e.target.value)
+  }
+
+  return (
+    <MentionsInput className="outline-none w-full max-w-full"
+      style={textareaStyle}
+      placeholder="Share your ideas, thought and creativity"
+      onChange={e => _onChange(e)}
+      value={text}
+      allowSuggestionsAboveCursor={true}
+      inputRef={inputRef}
+      autoFocus={autoFocus}
+    >
+      <Mention
+        trigger='@'
+        data={_getUsers}
+        appendSpaceOnAdd={true}
+        style={{
+          backgroundColor: `#df4544`,
+          borderRadius: `.1rem`
+        }}
+        renderSuggestion={RenderUser}
+      />
+    </MentionsInput>
+  )
+}
+
+export default RichText
--- src/components/NewPost/Text.js
+++ src/components/NewPost/Text.js
@@ -1,58 +1,6 @@
 import { useRef, useState, useEffect } from 'react'
-import { MentionsInput, Mention } from 'react-mentions'
-import near from '../../lib/near'
-import Image from '../Image'
 import Confirm from '../Utils/Confirm'
-
-const textareaStyle = {
-  control: {
-    fontSize: `16px`,
-    fontWeight: `500`,
-    color: '#616161'
-  },
-  highlighter: {
-    letterSpacing: `-.01rem`
-  },
-  input: {
-    margin: 0,
-    padding: `0 .5rem`,
-    color: `white`,
-    height: `100%`
-  },
-  suggestions: {
-    marginTop: `1rem`,
-    maxHeight: `8rem`,
-    overflowY: 'auto',
-    width: `100vw`,
-    maxWidth: `100%`,
-    boxShadow: `0px 0px 4px rgba(0, 0, 0, 0.15)`,
-    item: {
-      backgroundColor: `#121212`,
-      '&focused': {
-        backgroundColor: '#333333',
-      },
-    },
-  },
-}
-
-const RenderUser = (entry) => {
-  return (
-    <div className="flex items-center justify-between p-2">
-      <div className="w-8/12 flex items-center overflow-hidden">
-        <div>
-          <div className="w-8 h-8 rounded-full overflow-hidden">
-            <Image style={{
-              boxShadow: `0 0 4px 0px rgba(0, 0, 0, 0.75) inset`
-            }} className="object-cover w-full h-full" data={entry.imgAvatar} />
-          </div>
-        </div>
-        <div className="px-4 w-auto">
-          <p className="text-sm font-semibold text-white truncate whitespace-no-wrap min-w-0">{entry.username}</p>
-        </div>
-      </div>
-    </div>
-  )
-}
+import RichText from 'components/Input/RichText'
 
 const NewPostText = ({ left, right, input = '' }) => {
   const offsetY = 16
@@ -72,6 +20,9 @@ const NewPostText = ({ left, right, input = '' }) => {
       setTimeout(() => {
         setErr(false)
       }, 500)
+    }
+    else {
+      setCurText(textRaw)
     }
     if (textRaw.length > 0) {
       const lineCount = Math.round(inputRef.current.scrollHeight * 100 / maxHeight)
@@ -95,34 +46,8 @@ const NewPostText = ({ left, right, input = '' }) => {
     }
   }, [textRaw])
 
-  const _getUsers = async (query, callback) => {
-    if (!query) return
-    const q = [`username_like:=${query}`]
-    const userList = await near.contract.getUserList({
-      query: q,
-      opts: {
-        _embed: true,
-        _sort: 'createdAt',
-        _order: 'desc',
-        _limit: 10
-      }
-    })
-    const list = userList.map(user => ({
-      display: `@${user.username}`,
-      id: user.id,
-      imgAvatar: user.imgAvatar,
-      username: user.username
-    }))
-    callback(list)
-  }
-
   const _validateSubmit = () => {
     return textRaw.length > 0
-  }
-
-  const _onChange = (val) => {
-    setCurText(textRaw)
-    setTextRaw(val)
   }
 
   const _right = () => {
@@ -199,26 +124,13 @@ const NewPostText = ({ left, right, input = '' }) => {
                 ${err && 'animated shake'}
                 flex items-center h-full
               `}>
-                <MentionsInput className="outline-none w-full max-w-full"
-                  style={textareaStyle}
-                  placeholder="Share your ideas, thought and creativity"
-                  onChange={e => _onChange(e.target.value)}
-                  value={textRaw}
-                  allowSuggestionsAboveCursor={true}
-                  inputRef={inputRef}
-                  autoFocus
-                >
-                  <Mention
-                    trigger='@'
-                    data={_getUsers}
-                    appendSpaceOnAdd={true}
-                    style={{
-                      backgroundColor: `#df4544`,
-                      borderRadius: `.1rem`
-                    }}
-                    renderSuggestion={RenderUser}
-                  />
-                </MentionsInput>
+                <RichText 
+                  autoFocus 
+                  inputRef={inputRef} 
+                  text={textRaw} 
+                  setText={setTextRaw} 
+                  initialText={textRaw} 
+                />
               </div>
             </div>
           </div>
