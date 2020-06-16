064c760204e88dbb77f5756c3a191e213b5a1a0a
hdriqi <ra@evius.id>
2020-06-16 21:40:41 +0700

    fix add post on memento page

--- src/components/Memento/index.js
+++ src/components/Memento/index.js
@@ -30,18 +30,6 @@ const MementoData = ({ isNotFound, memento, isFollowing, isSubmitting, toggleFol
   if (memento) {
     return (
       <div className="p-4">
-        <div className="sticky block md:hidden" style={{
-          bottom: `2rem`
-        }}>
-          <Push href="/new/post" as="/new/post" props={{
-            memento: memento
-          }}>
-            <svg className="ml-auto mr-4" width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M56 28C56 43.464 43.464 56 28 56C12.536 56 0 43.464 0 28C0 12.536 12.536 0 28 0C43.464 0 56 12.536 56 28Z" fill="#E13128" />
-              <path fillRule="evenodd" clipRule="evenodd" d="M26.5292 38.6667V30.1375H18V26.5292H26.5292V18H30.1375V26.5292H38.6667V30.1375H30.1375V38.6667H26.5292Z" fill="white" />
-            </svg>
-          </Push>
-        </div>
         <div className="flex justify-center items-center">
           <div className="w-40 h-40 rounded-md overflow-hidden">
             <Image className="object-cover h-full" data={memento.img} />
@@ -139,6 +127,10 @@ const Memento = ({ memento, postListIds, postById, mementoById, getPost, hasMore
     setIsSubmitting(false)
   }
 
+  if (!memento) {
+    return null
+  }
+
   return (
     <div className="bg-dark-0 min-h-screen relative">
       <MementoModal
@@ -178,7 +170,7 @@ const Memento = ({ memento, postListIds, postById, mementoById, getPost, hasMore
             <MementoData isNotFound={memento.isNotFound} memento={mementoById[memento.id]} isFollowing={isFollowing} isSubmitting={isSubmitting} toggleFollow={_toggleFollow} setStickySubNav={setStickySubNav} />
           )
       }
-      <div>
+      <div className="pb-6">
         {
           postListIds ? (
             <InfiniteScroll
@@ -203,6 +195,19 @@ const Memento = ({ memento, postListIds, postById, mementoById, getPost, hasMore
               </div>
             )
         }
+      </div>
+      <div className="fixed block md:hidden" style={{
+        bottom: `2rem`,
+        right: `0`
+      }}>
+        <Push href="/new/post" as="/new/post" props={{
+          memento: memento
+        }}>
+          <svg className="ml-auto mr-4" width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M56 28C56 43.464 43.464 56 28 56C12.536 56 0 43.464 0 28C0 12.536 12.536 0 28 0C43.464 0 56 12.536 56 28Z" fill="#E13128" />
+            <path fillRule="evenodd" clipRule="evenodd" d="M26.5292 38.6667V30.1375H18V26.5292H26.5292V18H30.1375V26.5292H38.6667V30.1375H30.1375V38.6667H26.5292Z" fill="white" />
+          </svg>
+        </Push>
       </div>
     </div>
   )
