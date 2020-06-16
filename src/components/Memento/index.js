8bb0d5cd710f58e9c24818aef66d6bdae4e9b0d6
hdriqi <ra@evius.id>
2020-06-16 21:41:30 +0700

    only show add post on memento to logged in user

--- src/components/Memento/index.js
+++ src/components/Memento/index.js
@@ -196,19 +196,23 @@ const Memento = ({ memento, postListIds, postById, mementoById, getPost, hasMore
             )
         }
       </div>
-      <div className="fixed block md:hidden" style={{
-        bottom: `2rem`,
-        right: `0`
-      }}>
-        <Push href="/new/post" as="/new/post" props={{
-          memento: memento
-        }}>
-          <svg className="ml-auto mr-4" width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M56 28C56 43.464 43.464 56 28 56C12.536 56 0 43.464 0 28C0 12.536 12.536 0 28 0C43.464 0 56 12.536 56 28Z" fill="#E13128" />
-            <path fillRule="evenodd" clipRule="evenodd" d="M26.5292 38.6667V30.1375H18V26.5292H26.5292V18H30.1375V26.5292H38.6667V30.1375H30.1375V38.6667H26.5292Z" fill="white" />
-          </svg>
-        </Push>
-      </div>
+      {
+        me && me.id && (
+          <div className="fixed block md:hidden" style={{
+            bottom: `2rem`,
+            right: `0`
+          }}>
+            <Push href="/new/post" as="/new/post" props={{
+              memento: memento
+            }}>
+              <svg className="ml-auto mr-4" width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M56 28C56 43.464 43.464 56 28 56C12.536 56 0 43.464 0 28C0 12.536 12.536 0 28 0C43.464 0 56 12.536 56 28Z" fill="#E13128" />
+                <path fillRule="evenodd" clipRule="evenodd" d="M26.5292 38.6667V30.1375H18V26.5292H26.5292V18H30.1375V26.5292H38.6667V30.1375H30.1375V38.6667H26.5292Z" fill="white" />
+              </svg>
+            </Push>
+          </div>
+        )
+      }
     </div>
   )
 }
