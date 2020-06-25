9c8b7d1e27d864bacbb0a447c6fadb9bdaedef8d
hdriqi <ra@evius.id>
2020-06-25 12:19:08 +0700

    update default avatar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,10 +12,48 @@ import { setBalance } from "actions/wallet"
 import { NotifyContext } from "./Utils/NotifyProvider"
 import * as rax from 'retry-axios'
 
-const DEFAULT_AVATAR = {
-  url: 'QmbmkUNfVEQwUHzufSbC5nZQbdEMnNp6Hzfr88sQhZAois',
-  type: 'ipfs'
-}
+const DEFAULT_AVATAR = [
+  {
+    url: 'QmWvmboVv5wgApgwPcVns3HF3FKpgXSezqtkrJoq5xriCH',
+    type: 'ipfs'
+  },
+  {
+    url: 'Qmab3Umre1GmXuSnkh9wxdr4QNSVtV8DBTLQBRPFbJbUUS',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmfBxyRh5RwBAtB85Q6EGQEfNfprCDdz5mNu3HhLWynpo3',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmRSFZvUwWD61wyYeuYC2B9Z8JPxXXHFmTHeGeRFi7Bn7L',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmYBBFGBZV17oJb9HFQNB7EHmhDMo94GBPBdFnQEqSmF94',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmcUhG6UjqUYKydg7KoAmTok4TSwRXhrvCLb5gciSzatig',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmX6mc7wfDza8mDFMDGfmZsUQNZv1LhP8ztEURmHZf1xAF',
+    type: 'ipfs'
+  },
+  {
+    url: 'Qmdqg7bCjiKagvt421qhGdX5A6jXrAFVaUvxY6DwZMzLtE',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmYeGSYCn14ppxnP26D5p9W8NRgPEBqkfkYfoAULCfKhyE',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmQa68qLbW7iSSVNaS8o1rW9PiCYVoAneqn17D4oz6fbGu',
+    type: 'ipfs'
+  },
+]
 
 const SplashScreen = () => {
   return (
@@ -62,14 +100,6 @@ const Layout = ({ children }) => {
       if (typeof window !== 'undefined') {
         await near.init()
 
-        // try {
-        //   await near.contract.init({
-        //     initialOwner: 'riqi.testnet'
-        //   }) 
-        // } catch (err) {
-        //   console.log(err)
-        // }
-
         if (near.wallet.isSignedIn()) {
           const token = await near.authToken()
           rax.attach()
@@ -92,9 +122,10 @@ const Layout = ({ children }) => {
             id: near.currentUser.accountId
           })
           if (!profile) {
+            const avatar = DEFAULT_AVATAR[Math.floor(Math.random() * DEFAULT_AVATAR.length)]
             try {
               profile = await near.contract.createUser({
-                imgAvatar: DEFAULT_AVATAR,
+                imgAvatar: avatar,
                 bio: ''
               })
             } catch (err) {
