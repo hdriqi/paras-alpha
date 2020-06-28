44eb534ed08a5418fecb328594953d0329a17d66
hdriqi <ra@evius.id>
2020-06-28 19:43:31 +0700

    add New Memento on Profile -> View memento

--- src/components/Profile/Memento.js
+++ src/components/Profile/Memento.js
@@ -24,13 +24,17 @@ const ProfileMemento = ({ mementoList }) => {
           <h3 className="text-lg font-bold text-white px-2">My Memento</h3>
         }
         right={
-          <button onClick={_ => setShowModal(true)}>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M20 12C20 13.1046 19.1046 14 18 14C16.8954 14 16 13.1046 16 12C16 10.8954 16.8954 10 18 10C19.1046 10 20 10.8954 20 12Z" fill="#E2E2E2" />
-              <path d="M14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12Z" fill="#E2E2E2" />
-              <path d="M8 12C8 13.1046 7.10457 14 6 14C4.89543 14 4 13.1046 4 12C4 10.8954 4.89543 10 6 10C7.10457 10 8 10.8954 8 12Z" fill="#E2E2E2" />
-            </svg>
-          </button>
+          <Push
+            href="/new/memento"
+            as="/new/memento"
+          >
+            <button>
+              <svg width="24" height="24" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="16" cy="16" r="15" fill="#E13128" stroke="#E13128" strokeWidth="2" />
+                <path fillRule="evenodd" clipRule="evenodd" d="M14.5408 22.3337V17.4598H9.66699V14.5408H14.5408V9.66699H17.4598V14.5408H22.3337V17.4598H17.4598V22.3337H14.5408Z" fill="white" />
+              </svg>
+            </button>
+          </Push>
         }
       />
       <div className="px-4">
