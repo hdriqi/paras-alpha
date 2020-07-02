a0ec77f9535a32fca25896c9f13402b6008e4ec0
hdriqi <ra@evius.id>
2020-07-02 10:18:49 +0700

    update meta to each page

--- src/pages/[id].js
+++ src/pages/[id].js
@@ -10,9 +10,19 @@ const UserPage = () => {
   return (
     <Fragment>
       <Head>
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name="twitter:site" content="@ParasHQ" />
+        <meta name='twitter:url' content='https://alpha.paras.id' />
         <meta name='twitter:title' content={`${router.query.id} | Profile`} />
+        <meta name='twitter:description' content='Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.' />
+        <meta name='twitter:image' content='https://paras-media.s3-ap-southeast-1.amazonaws.com/paras-twitter-card-large.png' />
+
+        <meta property='og:type' content='website' />
         <meta property='og:title' content={`${router.query.id} | Profile`} />
         <meta property='og:site_name' content={`${router.query.id} | Profile`} />
+        <meta property='og:description' content='Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.' />
+        <meta property='og:url' content='https://alpha.paras.id' />
+        <meta property='og:image' content='https://paras-media.s3-ap-southeast-1.amazonaws.com/paras-twitter-card-large.png' />
       </Head>
       < ProfileScreen id={router.query.id} />
     </Fragment>
