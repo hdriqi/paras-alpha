51c60e04ab9d750f39dd1ea183145a3b24064bd4
hdriqi <ra@evius.id>
2020-07-02 09:59:46 +0700

    update meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,27 +13,27 @@ export default class MyDocument extends Document {
           <meta name='application-name' content='Paras' />
           <meta http-equiv='X-UA-Compatible' content='IE=edge' />
           <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-          <meta name='description' content='Decentralized collective memory' />
+          <meta name='description' content='Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.' />
           <meta name='keywords' content='decentralized social media,blockchain social media' />
 
           <link rel='manifest' href='/static/manifest.json' />
           <link href='/favicon.ico' rel='icon' type='image/png' sizes='16x16' />
-          <link rel='apple-touch-icon' href='/apple-icon.png'></link>
+          <link rel='apple-touch-icon' href='/favicon.ico'></link>
 
           <script async src="https://stat.paras.id/tracker.js" data-ackee-server="https://stat.paras.id" data-ackee-domain-id="889b9dbd-b828-43f8-a339-420b34b75d39"></script>
 
-          {/* <meta name='twitter:card' content='summary' />
-          <meta name='twitter:url' content='https://yourdomain.com' />
-          <meta name='twitter:title' content='PWA App' />
-          <meta name='twitter:description' content='Best PWA App in the world' />
-          <meta name='twitter:image' content='https://yourdomain.com/static/icons/android-chrome-192x192.png' />
-          <meta name='twitter:creator' content='@ParasHQ' />
+          <meta name='twitter:card' content='summary_large_image' />
+          <meta name="twitter:site" content="@ParasHQ" />
+          <meta name='twitter:url' content='https://alpha.paras.id' />
+          <meta name='twitter:title' content='Paras - Digital Collective Memory' />
+          <meta name='twitter:description' content='Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.' />
+          <meta name='twitter:image' content='https://paras-media.s3-ap-southeast-1.amazonaws.com/paras-twitter-card-large.png' />
           <meta property='og:type' content='website' />
-          <meta property='og:title' content='PWA App' />
-          <meta property='og:description' content='Best PWA App in the world' />
-          <meta property='og:site_name' content='PWA App' />
-          <meta property='og:url' content='https://yourdomain.com' />
-          <meta property='og:image' content='https://yourdomain.com/static/icons/apple-touch-icon.png' /> */}
+          <meta property='og:title' content='Paras - Digital Collective Memory' />
+          <meta property='og:description' content='Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.' />
+          <meta property='og:site_name' content='Paras - Digital Collective Memory' />
+          <meta property='og:url' content='https://alpha.paras.id' />
+          <meta property='og:image' content='https://paras-media.s3-ap-southeast-1.amazonaws.com/paras-twitter-card-large.png' />
         </Head>
         <body>
           <Main />
