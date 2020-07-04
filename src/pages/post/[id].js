ca0d656ed6fd190cc6f374123c35e93c17ec8a35
hdriqi <ra@evius.id>
2020-07-04 22:16:19 +0700

    fix error

--- src/pages/post/[id].js
+++ src/pages/post/[id].js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { withRedux } from '../../lib/redux'
 import PostScreen from '../../screens/PostScreen'
+import Head from 'next/head'
 
 const PostDetailPage = () => {
   const router = useRouter()
