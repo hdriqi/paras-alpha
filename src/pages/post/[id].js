6cb2e07c24e1371ec1d24e25796433c87ae5352e
hdriqi <ra@evius.id>
2020-07-08 18:54:27 +0700

    update meta tag

--- src/pages/post/[id].js
+++ src/pages/post/[id].js
@@ -1,41 +1,76 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
-import { withRedux } from '../../lib/redux'
+import axios from 'axios'
 import PostScreen from '../../screens/PostScreen'
 import Head from 'next/head'
 
-const PostDetailPage = () => {
-  const router = useRouter()
-  const [id, setId] = useState(null)
+const getDesc = (contentList) => {
+  const hasDesc = contentList.filter(content => {
+    return content.type === 'text'
+  })
+  if (hasDesc[0]) {
+    const data = hasDesc[0].body
+    return data
+  }
+  return null
+}
 
-  useEffect(() => {
-    if (router && router.query.id) {
-      setId(router.query.id)
+const getImgUrl = (contentList) => {
+  const hasThumbnailImg = contentList.filter(content => {
+    return content.type === 'img'
+  })
+  if (hasThumbnailImg[0]) {
+    const data = JSON.parse(hasThumbnailImg[0].body)
+    let url = ''
+    if (data && data.type === 'ipfs') {
+      url = `https://ipfs-gateway.paras.id/ipfs/${data.url}`
     }
-  }, [router])
+    else if (typeof data === 'string') {
+      url = data
+    }
+    return url
+  }
+  return null
+}
+
+const PostDetailPage = ({ id, post }) => {
+  const imgUrl = post && getImgUrl(post.contentList)
+  const desc = post && getDesc(post.contentList)
 
   return (
     <div>
       <Head>
-      <title>Paras - Digital Collective Memory</title>
-        <meta name="description" content='Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.' />
-        
-        <meta name='twitter:title' content='Paras - Digital Collective Memory' />
-        <meta name='twitter:card' content='summary_large_image' />
+        <title>{`${post.owner} on Paras`}</title>
+        <meta name="description" content={desc || 'Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.'} />
+
+        <meta name='twitter:title' content={`${post.owner} on Paras`} />
+        <meta name='twitter:card' content='summary' />
         <meta name="twitter:site" content="@ParasHQ" />
         <meta name='twitter:url' content='https://alpha.paras.id' />
-        <meta name='twitter:description' content='Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.' />
-        <meta name='twitter:image' content='https://paras-media.s3-ap-southeast-1.amazonaws.com/paras-twitter-card-large.png' />
+        <meta name='twitter:description' content={desc || 'Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.'} />
+        <meta name='twitter:image' content={imgUrl || 'https://paras-media.s3-ap-southeast-1.amazonaws.com/paras-twitter-card-large.png'} />
         <meta property='og:type' content='website' />
         <meta property='og:title' content='Paras - Digital Collective Memory' />
         <meta property='og:site_name' content='Paras - Digital Collective Memory' />
-        <meta property='og:description' content='Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.' />
+        <meta property='og:description' content={desc || 'Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.'} />
         <meta property='og:url' content='https://alpha.paras.id' />
-        <meta property='og:image' content='https://paras-media.s3-ap-southeast-1.amazonaws.com/paras-twitter-card-large.png' />
+        <meta property='og:image' content={imgUrl || 'https://paras-media.s3-ap-southeast-1.amazonaws.com/paras-twitter-card-large.png'} />
       </Head>
       <PostScreen id={id} />
     </div>
   )
 }
 
-export default withRedux(PostDetailPage)
+export async function getServerSideProps(context) {
+  const id = context.params.id
+  const response = await axios.get(`${process.env.BASE_URL}/posts?id=${id}`)
+  const post = response.data.data[0]
+  return {
+    props: {
+      id: context.params.id,
+      post: post || null
+    },
+  }
+}
+
+export default PostDetailPage
--- src/pages/wallet/index.js
+++ src/pages/wallet/index.js
@@ -1,7 +1,29 @@
 import WalletScreen from 'screens/WalletScreen'
+import Head from 'next/head'
 
 const WalletPage = () => {
-  return <WalletScreen />
+  return (
+    <div>
+      <Head>
+        <title>Wallet - Paras</title>
+        <meta name="description" content='Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.' />
+
+        <meta name='twitter:title' content='Paras - Digital Collective Memory' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name="twitter:site" content="@ParasHQ" />
+        <meta name='twitter:url' content='https://alpha.paras.id' />
+        <meta name='twitter:description' content='Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.' />
+        <meta name='twitter:image' content='https://paras-media.s3-ap-southeast-1.amazonaws.com/paras-twitter-card-large.png' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Paras - Digital Collective Memory' />
+        <meta property='og:site_name' content='Paras - Digital Collective Memory' />
+        <meta property='og:description' content='Meet like-minded people and discover new ideas, thoughts, and creativity. Share, explore, and think independently.' />
+        <meta property='og:url' content='https://alpha.paras.id' />
+        <meta property='og:image' content='https://paras-media.s3-ap-southeast-1.amazonaws.com/paras-twitter-card-large.png' />
+      </Head>
+      <WalletScreen />
+    </div>
+  )
 }
 
 export default WalletPage
